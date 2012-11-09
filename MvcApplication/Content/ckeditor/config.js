9b947c300155f1a4b52044b9d07b8c6db24b31c6
alexander.panfilenok <alexander.panfilenok@f8f79e24-c20c-8775-e4e3-ce57b7db7bff>
2012-11-09 09:44:17 +0000

    Ckeditor toolbar change
    git-svn-id: https://github.com/lAnubisl/aspnet-mvc-application.git/trunk@21 f8f79e24-c20c-8775-e4e3-ce57b7db7bff

--- MvcApplication/Content/ckeditor/config.js
+++ MvcApplication/Content/ckeditor/config.js
@@ -3,8 +3,17 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function(config) {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+	[
+		{ name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+		{ name: 'basicstyles', items: ['Bold', 'Italic'] },
+	    { name: 'insert', items : [ 'Image' ] },
+		{ name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+		{ name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+		{ name: 'tools', items: ['Maximize'] },
+	    { name: 'document', items: ['Source'] }
+	];
 };
