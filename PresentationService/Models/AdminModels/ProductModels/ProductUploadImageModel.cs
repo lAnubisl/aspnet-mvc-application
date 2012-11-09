1bf3d1765c5b9ae8a4b045cef2fb484ca37469b1
alexander.panfilenok <alexander.panfilenok@f8f79e24-c20c-8775-e4e3-ce57b7db7bff>
2012-11-09 11:09:29 +0000

    Upload Image Validation
    git-svn-id: https://github.com/lAnubisl/aspnet-mvc-application.git/trunk@22 f8f79e24-c20c-8775-e4e3-ce57b7db7bff

--- PresentationService/Models/AdminModels/ProductModels/ProductUploadImageModel.cs
+++ PresentationService/Models/AdminModels/ProductModels/ProductUploadImageModel.cs
@@ -1,9 +1,24 @@
-﻿namespace PresentationService.Models.AdminModels.ProductModels
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace PresentationService.Models.AdminModels.ProductModels
 {
-    public class ProductUploadImageModel
+
+    public class ProductUploadImageModel : IValidatableObject
     {
+        public HttpPostedFileBase File { get; set; }
+
         public string ImageUrl { get; set; }
 
         public bool Success { get; private set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (File == null || string.IsNullOrEmpty(ImageUrl))
+            {
+                yield return new ValidationResult("Вы должны или выбрать файл изображения или ввести URL на изображение.");
+            }
+        }
     }
 }
