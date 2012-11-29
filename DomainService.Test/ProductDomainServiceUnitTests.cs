3bb42fc9f14dc8a1a5c1b20565103b513bb9a4e1
alexander.panfilenok <alexander.panfilenok@f8f79e24-c20c-8775-e4e3-ce57b7db7bff>
2012-11-29 17:04:23 +0000

    Create Unit Test Project
    git-svn-id: https://github.com/lAnubisl/aspnet-mvc-application.git/trunk@40 f8f79e24-c20c-8775-e4e3-ce57b7db7bff

---
+++ DomainService.Test/ProductDomainServiceUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using DomainService.DomainModels;
+using DomainService.DomainServices;
+using DomainService.RepositoryInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+namespace DomainService.Test
+{
+    [TestClass]
+    public class ProductDomainServiceUnitTests
+    {
+        [TestMethod]
+        public void LoadByNameShouldReturnProductByName()
+        {
+            var fakeDB = new Mock<IGenericRepository<Product>>();
+            fakeDB.Setup(x => x.Query()).Returns(new List<Product>
+                {
+                    new Product { Name = "Product A" },
+                    new Product { Name = "Product B" },
+                    new Product { Name = "Product C" }
+                }.AsQueryable);
+
+            var service = new ProductDomainService {Repository = fakeDB.Object};
+            Assert.IsTrue(service.LoadByName("Product A") != null);
+            Assert.IsTrue(service.LoadByName("Product D") == null);
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void LoadByNameShouldThrowExceptionInCaseOfTwoProductsWithTheSameName()
+        {
+            var fakeDB = new Mock<IGenericRepository<Product>>();
+            fakeDB.Setup(x => x.Query()).Returns(new List<Product>
+                {
+                    new Product { Name = "Product A" },
+                    new Product { Name = "Product A" }
+                }.AsQueryable);
+
+            var service = new ProductDomainService { Repository = fakeDB.Object };
+            service.LoadByName("Product A");
+        }
+
+        [TestMethod]
+        public void LoadProductsForTermShouldReturnProductsByTerm()
+        {
+            var fakeDB = new Mock<IGenericRepository<Product>>();
+            fakeDB.Setup(x => x.Query()).Returns(new List<Product>
+                {
+                    new Product { Name = "Red bicycle" },
+                    new Product { Name = "Yellow umbrella" },
+                    new Product { Name = "White pillow" },
+                    new Product { Name = "Red pillow" },
+                    new Product { Name = "Red umbrella" },
+                    new Product { Name = "White umbrella" },
+                    new Product { Name = "Black redemption" }
+                }.AsQueryable);
+            var service = new ProductDomainService { Repository = fakeDB.Object };
+            Assert.IsTrue(service.LoadProductsForTerm("Red").Count() == 4);
+        }
+    }
+}
