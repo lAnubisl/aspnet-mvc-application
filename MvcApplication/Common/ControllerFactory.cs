f1268490fecbd835065649e41becd13b8223c552
alexander.panfilenok <alexander.panfilenok@f8f79e24-c20c-8775-e4e3-ce57b7db7bff>
2013-01-25 09:29:36 +0000

    Add ControllerFactory.cs
    git-svn-id: https://github.com/lAnubisl/aspnet-mvc-application.git/trunk@47 f8f79e24-c20c-8775-e4e3-ce57b7db7bff

---
+++ MvcApplication/Common/ControllerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using PresentationService;
+
+namespace MvcApplication.Common
+{
+    public class ControllerFactory : DefaultControllerFactory
+    {
+        public override void ReleaseController(IController controller)
+        {
+            var disposable = controller as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+
+            IOC.Release(controller);
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return (IController)IOC.Resolve(controllerType);
+        }
+    }
+}
